e4fee5a6d52aa1b30768140462dc9c9d3876aa3c
LaurensvanG <Laurens.van.Giersbergen@gmail.com>
2021-10-08 22:19:22 +0200

    Pigment page modal working

--- app.py
+++ app.py
@@ -1,44 +1,66 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-
-
-
-@app.route("/")
-def index():
-
-    BOARDS = dict(
+BOARDS = dict(
         bw = dict (
             img = "black.png",
-            url = "/bw",
+            url = "bw",
             text = "Black & White",
+            logo = "circle_dual_adjusted_edit.svg",
         ),
         blue = dict (
             img = "blue.png",
             url = "blue",
             text = "Blue",
+            logo = "circle_edit.svg",
         ),
         red = dict (
             img = "red.png",
             url = "red",
             text = "Red",
+            logo = "circle_edit.svg",
         ),
         green = dict (
             img = "green.png",
             url = "green",
             text = "Green",
+            logo = "circle_edit.svg",
         ),
         yellow = dict (
             img = "yellow.png",
             url = "yellow",
             text = "Yellow",
+            logo = "circle_edit.svg",
         ),
         brown = dict (
             img = "brown.png",
             url = "brown",
             text = "Brown",
+            logo = "circle_edit.svg",
         )
     )
 
-    return render_template("index.html", boards=BOARDS)
+
+@app.context_processor
+def create_menu():
+
+    return dict(BOARDS=BOARDS)
+
+
+
+@app.route("/", methods=["GET"])
+def index():
+
+    return render_template("index.html")
+
+
+@app.route("/board/<board>", methods=["GET"])
+def board(board):
+
+    return render_template(f"{board}.html")
+
+@app.route("/board", methods=["GET"])
+def board_only():
+
+    return redirect("/")
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,17 +1,19 @@
-// Adapted from https://levelup.gitconnected.com/overcoming-css-not-calculating-auto-height-for-transitions-f98a7e062f07
-window.addEventListener("DOMContentLoaded", () => (function(d, w) {
-  console.log(d, w)
+// Wait for content to be loaded
+window.addEventListener("DOMContentLoaded", () => ((d, w) => {
 
+    // Flyout menu items
+    // Adapted from https://levelup.gitconnected.com/overcoming-css-not-calculating-auto-height-for-transitions-f98a7e062f07
+    
     // Grab all targets to be resized
-    var calcTargets = d.getElementsByClassName("resize-container");
+    let calcTargets = d.getElementsByClassName("resize-container");
     
     // Make sure the variable --calc-height on body is set to auto
     d.body.style.setProperty("--calc-height", "auto");
 
     // Set the current width of the element to the calculated auto width
     function resize() {
-      for (var target of calcTargets) {
-        var width = target.firstElementChild.offsetWidth + "px";
+      for (let target of calcTargets) {
+        let width = target.firstElementChild.offsetWidth + "px";
         if (target.style.getPropertyValue("--calc-width") !== width) {
           target.style.setProperty("--calc-width", width);
         }
@@ -20,5 +22,41 @@ window.addEventListener("DOMContentLoaded", () => (function(d, w) {
     
     w.addEventListener("resize", resize, false);
     w.addEventListener("load", resize, false);
+
+
+    // Modal
+    const modal = d.getElementById("test");
+
+    // Get the button that opens the modal
+    const btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the pigment, open the modal
+    modal.onclick = function() {
+      modal.classList.add("modal");
+
+      // Disable hover effects
+      modal.classList.remove("pigment-hover");
+
+      modal.firstElementChild.classList.add("container", "card");
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.classList.remove("modal");
+        modal.classList.add("pigment-hover");
+      modal.firstElementChild.classList.remove("container");
+
+      }
+    }
+
   })(document, window)
 )
